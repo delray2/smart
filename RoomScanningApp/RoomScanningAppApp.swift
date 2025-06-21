e8876fac434dd7b3434109a97de8910f2fb061ee
Jaocb Delvoye <delvjacob@icloud.com>
2025-06-20 19:45:49 -0700

    Initial Commit

---
+++ RoomScanningApp/RoomScanningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomScanningAppApp.swift
+//  RoomScanningApp
+//
+//  Created by Jake Delvoye on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RoomScanningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
